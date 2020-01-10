be3de8c0ff5f404a3c04ad6d30256d4e49263540
Nicklas Spendler <xdanishgamerz@gmail.com>
2020-01-10 12:20:21 +0100

    install gulp dependicies

--- index.js
+++ index.js
@@ -2,87 +2,95 @@
 
 const fs = require('fs');
 
-let packageFolder = `${__dirname}/pkg`
+var MODE_0666 = parseInt('0755', 8);
 
-let content = fs.readdirSync(packageFolder)
+let { exec } = require("child_process");
 
-let targetPath = `${__dirname}`
+let packageFolder = "./pkg"
 
-// Uses regex to substring the extended path only
-function getExtendedPath(path){
-    let extraPathReg = path.match(/\/pkg\/.*/);
-    return extraPathReg[0].substring(5,extraPathReg[0].length)
-}
+// Example on how to call the function,
+// Folder-to-copy, target-folder, Name-of-folder-to-copy
+copyFolder(packageFolder, "src", "pkg")
+
+function copyFolder(fromDir, toDir, firstFolderName) {
+
+    let content = fs.readdirSync(fromDir)
+
+    // Uses regex to substring the extended path only
+    function getExtendedPath(path) {
+        let tempRegex = new RegExp(`\/${firstFolderName}\/.*`)
+        let extraPathReg = path.match(tempRegex);
+        return extraPathReg[0].substring(5, extraPathReg[0].length)
+    }
 
 
-initRepackFolder()
-function initRepackFolder(){
-    if(fs.existsSync(`${__dirname}/repack`)){
-        console.log('repack mappen findes', )
-    }else{
-        console.log('Repack mappen findes ikke, laver repack mappen', )
-        try{
-            fs.mkdirSync(`${__dirname}/repack`)
-            console.log('Repack mappen er lavet:', `${__dirname}/repack`)
-        } catch (err){
-            console.log('Kunne ikke lave repack mappen, wtf?', )
+    initRepackFolder()
+    function initRepackFolder() {
+        if (fs.existsSync(`${__dirname}/${toDir}`)) {
+            console.log(`${toDir} mappen findes`)
+        } else {
+            console.log('Repack mappen findes ikke, laver repack mappen')
+            try {
+                fs.mkdirSync(`${__dirname}/${toDir}`)
+                console.log('Repack mappen er lavet:', `${__dirname}/${toDir}`)
+            } catch (err) {
+                console.log(`Kunne ikke lave ${toDir} mappen, wtf?`)
+            }
         }
     }
-}
 
-content.forEach(folder => {
-    const folderPath = `${__dirname}/pkg/${folder}`
-    copyFolder(folderPath, "repack")
-});
-
-// Copies folders and check if theres more folders.
-function copyFolder(path, targetFolder){
-    // path = path to the dir its currenctly foreaching
-    let tempDir = fs.readdirSync(path) //Shows content of a folder
-    tempDir.forEach(file => {
-        if(fs.lstatSync(path).isDirectory()){
-            let extraPathReg = path.match(/\/pkg\/.*/);
-            let extraPath = getExtendedPath(path)
-            // console.log('dir', path)
-            // console.log('', extraPath)
-            // console.log(`${targetFolder}/${extraPath}`)
-            if(fs.existsSync(`${targetFolder}/${extraPath}`)){
-                // console.log('Findes allerede', )
-            }else{
-                // console.log('', `${__dirname}/${targetFolder}/${extraPath}`)
-                // console.log('findes ikke', )
-                fs.mkdirSync(`${__dirname}/${targetFolder}/${extraPath}`)
-            }
-        }
-        if(fs.lstatSync(`${path}/${file}`).isDirectory()){
-            let extraPathReg = path.match(/\/pkg\/.*/);
-            let extraPath = getExtendedPath(path)
-            // console.log('', path)
-            // console.log('', extraPath)
-            // console.log(`/${targetFolder}/${extraPath}/${file}`)
-            // console.log('', `${__dirname}/${targetFolder}/${file}`)
-
-            if(fs.existsSync(`${__dirname}/${targetFolder}/${file}`)){
-                copyFolder(`${path}/${file}`, targetFolder);
-            }else{
-                fs.mkdirSync(`${__dirname}/${targetFolder}/${file}`)
-                copyFolder(`${path}/${file}`, targetFolder);
-            }
-            
-            
-        }else{
-            // console.log('', file)
-            // console.log('', path)
-            let extraPath = getExtendedPath(path)
-            // console.log('', extraPath)
-            // console.log('', `${__dirname}/${targetFolder}/${extraPath}/${file}`)
-            // console.log('', `${path}/${file}`)
-            
-            try{
-                fs.copyFileSync(`${path}/${file}`, `${__dirname}/${targetFolder}/${extraPath}/${file}`)
-            } catch {
-                console.log('Er du sikker på at folderne findes? Err måske start copyFolder function', )
-            }
-        }
+    content.forEach(folder => {
+        const folderPath = `${fromDir}/${folder}`
+        copyFolder(folderPath, toDir, folder)
     });
+
+    // Copies folders and check if theres more folders.
+
+    // 
+    function copyFolder(path, targetFolder, folderName) {
+        // path = path file/folder its currently foreaching
+        if (!fs.lstatSync(path).isDirectory()) {
+            fs.copyFileSync(path, `./${targetFolder}/${folderName}`)
+        } else {
+            let tempDir = fs.readdirSync(path) //Shows content of a folder
+            tempDir.forEach(file => {
+                if (fs.lstatSync(path).isDirectory()) {
+                    let extraPath = getExtendedPath(path)
+                    if (fs.existsSync(`${targetFolder}/${extraPath}`)) {
+                        // console.log('Findes allerede', )
+                    } else {
+                        // console.log('findes ikke', )
+                        fs.mkdirSync(`${__dirname}/${targetFolder}/${extraPath}`)
+                    }
+                }
+                if (fs.lstatSync(`${path}/${file}`).isDirectory()) {
+                    if (fs.existsSync(`${__dirname}/${targetFolder}/${file}`)) {
+                        copyFolder(`${path}/${file}`, targetFolder);
+                    } else {
+                        fs.mkdirSync(`${__dirname}/${targetFolder}/${file}`)
+                        copyFolder(`${path}/${file}`, targetFolder);
+                    }
+                } else {
+                    let extraPath = getExtendedPath(path)
+                    try {
+                        fs.copyFileSync(`${path}/${file}`, `${__dirname}/${targetFolder}/${extraPath}/${file}`)
+                    } catch {
+                        console.log('Er du sikker på at folderne findes? Err måske start copyFolder function')
+                    }
+                }
+            });
+        }
+    }
+
 }
+
+exec("npm i -D @babel/core @babel/preset-env gulp gulp-babel gulp-clean-css gulp-concat gulp-connect gulp-imagemin gulp-pug gulp-rename gulp-sass gulp-sourcemaps gulp-uglify imagemin-jpeg-recompress", (err, stdout, stderr)=>{
+    if(err){
+        console.log('', err)
+        process.exit(1);
+    }
+
+    stdout.normalize("data", ()=>{
+        console.log('', dat.toString())
+    })
+})
